5e740eef7e62f8b5c9c17d222f3442616fab3d18
Maulik Dodia <mmdoida@gmail.com>
2025-09-07 15:23:17 +0530

    Improve ui elements

--- app/src/main/java/com/noteapp/presentation/ui/screen/NoteListScreen.kt
+++ app/src/main/java/com/noteapp/presentation/ui/screen/NoteListScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -74,13 +75,8 @@ fun NoteListScreen(viewModel: NoteListViewModel, onDeleteAllNotesClick: () -> Un
                 },
                 actions = {
                     if (hasNotes) {
-                        IconButton(onClick = {
-                            showDeleteAllNotesDialog = true
-                        }) {
-                            Icon(
-                                imageVector = Icons.Default.Delete,
-                                contentDescription = stringResource(id = R.string.delete)
-                            )
+                        TextButton(onClick = { showDeleteAllNotesDialog = true }) {
+                            Text(text = stringResource(id = R.string.delete_all))
                         }
                     }
                 }
