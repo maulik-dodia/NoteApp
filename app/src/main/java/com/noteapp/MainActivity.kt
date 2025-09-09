b148b74a44f7c74b342229283ae01458ecd53235
Maulik Dodia <mmdoida@gmail.com>
2025-09-09 19:01:51 +0530

    Implement dagger 2 di

--- app/src/main/java/com/noteapp/MainActivity.kt
+++ app/src/main/java/com/noteapp/MainActivity.kt
@@ -4,21 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.noteapp.data.repository.NoteRepositoryImpl
+import com.noteapp.data.repository.NoteRepository
 import com.noteapp.presentation.navigation.NoteNavigation
+import com.noteapp.presentation.viewmodel.NoteListViewModelFactory
 import com.noteapp.ui.theme.NoteAppTheme
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        val db = (application as NoteApp).database
-        val noteRepository = NoteRepositoryImpl(noteDao = db.noteDao())
+    @Inject
+    lateinit var noteRepository: NoteRepository
+
+    @Inject
+    lateinit var noteListViewModelFactory: NoteListViewModelFactory
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        (application as NoteApp).appComponent.inject(activity = this)
+        super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
         setContent {
             NoteAppTheme {
-                NoteNavigation(noteRepository = noteRepository)
+                NoteNavigation(
+                    noteRepository = noteRepository,
+                    noteListViewModelFactory = noteListViewModelFactory
+                )
             }
         }
     }
